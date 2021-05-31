20b65b3782215456a5ebefb9ea789f36fcb5bebc
Dharmik Patel <ddp10048971@gmail.com>
2021-05-31 09:17:24 -0400

    Added PQ and re calc HCost

--- main.py
+++ main.py
@@ -1,53 +1,119 @@
-from pygame import draw
+from pygame import time
 from node import Node
 import pygame
-from pygame import Rect
+from pygame import *
+from pygame import draw
+from queue import PriorityQueue
+import time
+from pygame.locals import *
+from pygame import font
+from pygame import display
 
 pygame.init()
-screen = pygame.display.set_mode([900, 900])
+screen = display.set_mode([1000, 900])
 BACKGROUND_COLOR = (37, 59, 89)
 STARTING_NODE_COLOR = (217, 200, 119)
 ENDING_NODE_COLOR = (217, 174, 137)
 WHITE = "white"
-
+font1: font.Font = font.SysFont("Arial", 15)
 
 
 
 maze = []
+startingNode = None
+
+
+
 def createMaze(x, y, r, c, start, end):  
     for i in range(r):
         maze.append([])
         for j in range(c):
             color = BACKGROUND_COLOR
+            type = Node.TYPE_EMPTY
             if (i, j) == start:
                 color = STARTING_NODE_COLOR
+                type = Node.TYPE_START
             elif (i, j) == end:
                 color = ENDING_NODE_COLOR
-            maze[i].append(Node(Node.SIZE*i, Node.SIZE*j, color, Node.EMPTY))
+                type = Node.TYPE_END
+            maze[i].append(Node(i, j, color, type))
+
 def drawMaze():
     for i in range(len(maze)):
         for j in range(len(maze[i])):
             draw.rect(screen, maze[i][j].color, maze[i][j].rect)
+            imgFont = font1.render(f" {maze[i][j].getGCost()}+{maze[i][j].getHCost()}={str(maze[i][j].getFCost()) + maze[i][j].getArrow()}", True, WHITE)
+            screen.blit(imgFont, maze[i][j].rect)
+    
     start = 0
     end = len(maze[0])*Node.SIZE
     for i in range(len(maze)):
         draw.line(screen, WHITE, (Node.SIZE*i, start), (Node.SIZE*i, end))
     draw.line(screen, WHITE, (Node.SIZE*(i+1), start), (Node.SIZE*(i+1), end))
+
     start = 0
     end = len(maze)*Node.SIZE
     for i in range(len(maze[0])):
         draw.line(screen, WHITE, (start, Node.SIZE*i), (end, Node.SIZE*i))
     draw.line(screen, WHITE, (start, Node.SIZE*(i+1)), (end, Node.SIZE*(i+1)))
-  
+    
+
 createMaze(0, 0, 10, 10, (2, 4), (9, 9))
 
+open = PriorityQueue() #all the nodes that r looked at
+open.put(Node.START_NODE)
+closed = [] #currnt path list, more exlcuisve, so all the nodes that cause expansion
 
-running = True
+"""
+for i in range(max(Node.START_NODE.x-1, 0), min(Node.START_NODE.x+2, len(maze))):
+    for j in range(max(Node.START_NODE.y-1, 0), min(Node.START_NODE.y+2, len(maze[i]))):
+        curNode: Node = maze[i][j]
+        if curNode is Node.START_NODE:
+            continue
+        print((i, j))
+        """
+def Astar() -> bool:
+    bestNode: Node = open.get(timeout=1)
+    bestNode.color = "red"
+    print(f"Best Node {bestNode}")
+    if bestNode is Node.END_NODE:
+        return True
+    closed.append(bestNode)
+    for i in range(max(bestNode.x-1, 0), min(bestNode.x+2, len(maze))):
+        for j in range(max(bestNode.y-1, 0), min(bestNode.y+2, len(maze[i]))):
+            curNode: Node = maze[i][j]
+            if curNode is bestNode or curNode in closed:
+                continue
+            curNode.color = "blue"
+            if curNode in open.queue:
+                tempParrent: Node = curNode.parrent
+                tempGCost: int = curNode.getGCost()
+                curNode.parrent = bestNode
+                if tempGCost < curNode.getGCost():
+                    curNode.parrent = tempParrent
+                else:
+                    open.put(open.get())
+            else:
+                curNode.parrent = bestNode
+                open.put(curNode)
+    return False
+    
+                
+                
+
+
+running: bool = True
+winnerFound: bool = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill(BACKGROUND_COLOR)
+    if not winnerFound:
+        winnerFound = Astar()
+    if winnerFound:
+        print("Winner found")
     drawMaze()
     pygame.display.flip()
+    time.sleep(1)
 pygame.quit()
--- node.py
+++ node.py
@@ -1,14 +1,70 @@
-from pygame import Rect
+from pygame import Rect, math
+from functools import total_ordering
+import queue
+import math
 
 
+@total_ordering
 class Node:
-    WALL = 0
-    EMPTY = 1
-    SIZE = 30
-    def __init__(self, x, y, color, type):
-        self.x = x
-        self.y = y
+    TYPE_WALL = 0
+    TYPE_EMPTY = 1
+    TYPE_START = 2
+    TYPE_END = 3
+    START_NODE = None
+    END_NODE = None
+    SIZE = 90
+    def __init__(self, x, y, color, type, g = 0, h = 0):
+        self.x: int = x
+        self.y: int = y
         self.color = color
-        self.type = type
-        self.rect = Rect(x, y, Node.SIZE, Node.SIZE)
+        self.type: int = type
+        self.rect: Rect = Rect(Node.SIZE*x, Node.SIZE*y, Node.SIZE, Node.SIZE)
+        if type == Node.TYPE_START:
+            Node.START_NODE = self
+        elif type == Node.TYPE_END:
+            Node.END_NODE = self
+        self.parrent: Node = None
+    def getArrow(self) -> str:
+        if self.parrent == None:
+            return ""
+        if self.parrent.x == self.x-1 and self.parrent.y == self.y-1:
+            return "←↑"
+        if self.parrent.x == self.x and self.parrent.y == self.y-1:
+            return "↑"
+        if self.parrent.x == self.x+1 and self.parrent.y == self.y-1:
+            return "→↑"
+        if self.parrent.x == self.x+1 and self.parrent.y == self.y:
+            return "→"
+        if self.parrent.x == self.x+1 and self.parrent.y == self.y+1:
+            return "→↓"
+        if self.parrent.x == self.x and self.parrent.y == self.y+1:
+            return "↓"
+        if self.parrent.x == self.x-1 and self.parrent.y == self.y+1:
+            return "←↓"
+        if self.parrent.x == self.x-1 and self.parrent.y == self.y:
+            return "←"
+
+    def getGCost(self):
+        if self.type != Node.TYPE_START and self.parrent != None:
+            return self.parrent.getGCost() + int(math.sqrt((self.parrent.y - self.y)**2 + (self.parrent.x - self.x)**2)*10)
+        return 0
+    def getHCost(self):
+        return int(math.sqrt((Node.END_NODE.y - self.y)**2 + (Node.END_NODE.x - self.x)**2)*10)
+    def getFCost(self):
+        return self.getGCost() + self.getHCost()
+
+    def __eq__(self, other) -> bool:
+        return False
+        '''
+        if other == None:
+            return False
+        return (self.getFCost(), self.getHCost(), self.getGCost()) == (other.getFCost(), other.getHCost(), other.getGCost())
+        '''
+    def __lt__(self, other) -> bool:
+        if (self.getFCost(), self.getHCost()) == (other.getFCost(), other.getHCost()):
+            return self.getGCost() > other.getGCost()
+        else:
+            return (self.getFCost(), self.getHCost()) < (other.getFCost(), other.getHCost())
+    def __str__(self) -> str:
+        return f"({self.getFCost()}, {self.getHCost()}, {self.getGCost()})\t ({self.x}, {self.y})"
     
